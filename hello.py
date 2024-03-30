e07cb86957d62a2493cac7f3e05a4025ee5e056f
Serghei <siracencoserghei@gmail.com>
2024-03-30 20:00:57 +0100

    some adds

--- hello.py
+++ hello.py
@@ -1,4 +1,16 @@
-from fastapi import  FastAPI
+"""
+Which Method Is Best?
+Here are a few recommendations:
+• When passing arguments in the URL, following RESTful guidelines is standard
+practice.
+• Query strings are usually used to provide optional arguments, like pagination.
+• The body is usually used for larger inputs, like whole or partial models.
+In each case, if you provide type hints in your data definitions, your arguments will
+be automatically type-checked by Pydantic. This ensures that they’re both present and
+correct.
+"""
+import requests
+from fastapi import  FastAPI,Request, Response, Header, Query, Body, HTTPException
 import uvicorn
 
 app = FastAPI()
@@ -28,10 +40,38 @@ app = FastAPI()
 
 # ========= third part =======
 
+# @app.get("/hi")
+# def greet():
+#     return "Hello World"
+#
+# @app.get("/hi/{who}")
+# def greet_with_name(who: str):
+#     return f"Hello {who}!"
 @app.get("/hi")
-def greet(who):
-    return f"Hello? {who}?"
+def greet(who: str = Query(None, description="Who to greet"), body: str = Body(None),  header: str = Header(None)):
+    print(f"{body=} {header=} {who=}")
+    if who:
+        return f"Hello? {who}?"
+    else:
+        return "Hello World"
 
+# @app.post("/agent")
+# def get_agent(user_agent: str = Header(None, convert_underscores=False)):
+#     return user_agent
+
+@app.get("/agent")  # http://0.0.0.0:8000/agent?user_agent=Hallo%20World
+def get_agent(user_agent: str = Query(None, description="User-Agent")):
+    return user_agent
+
+@app.get("/happy")
+def happy():
+    return Response(content=";)", status_code=250)
+
+@app.get("/header/{name}/{value}")
+def header(name: str, value: str, response: Response, request: Request):
+    response.headers[name] = value
+    print(f"{value=} {name=} {request.scope=} {response.headers=}", end='\n\p')
+    return "normal body"
 
 if __name__ == "__main__":
     uvicorn.run("hello:app",
